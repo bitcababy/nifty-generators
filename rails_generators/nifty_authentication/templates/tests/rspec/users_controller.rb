02d9fc2405d8d6924fc597b47d0b10b71e371c1f
Meredith Lesly <bitcababy+github@gmail.com>
2011-08-18 16:59:16 -0400

    Rewritten to be more readable

--- rails_generators/nifty_authentication/templates/tests/rspec/users_controller.rb
+++ rails_generators/nifty_authentication/templates/tests/rspec/users_controller.rb
@@ -4,23 +4,27 @@ describe <%= user_plural_class_name %>Controller do
   fixtures :all
   integrate_views
 
-  it "new action should render new template" do
-    get :new
-    response.should render_template(:new)
-  end
+	describe "#new" do
+	  it "should render new template" do
+	    get :new
+	    response.should render_template(:new)
+	  end
+	end
 
-  it "create action should render new template when model is invalid" do
-    <%= user_class_name %>.any_instance.stubs(:valid?).returns(false)
-    post :create
-    response.should render_template(:new)
-  end
+	describe "#create" do
+	  it "should render new template when model is invalid" do
+	    <%= user_class_name %>.any_instance.stubs(:valid?).returns(false)
+	    post :create
+	    response.should render_template(:new)
+	  end
 
-  it "create action should redirect when model is valid" do
-    <%= user_class_name %>.any_instance.stubs(:valid?).returns(true)
-    post :create
-    response.should redirect_to(root_url)
-  <%- unless options[:authlogic] -%>
-    session['<%= user_singular_name %>_id'].should == assigns['<%= user_singular_name %>'].id
-  <%- end -%>
-  end
+	  it "should redirect when model is valid" do
+	    <%= user_class_name %>.any_instance.stubs(:valid?).returns(true)s
+	    post :create
+	    response.should redirect_to(root_url)
+	  <%- unless options[:authlogic] -%>
+	    session['<%= user_singular_name %>_id'].should == assigns['<%= user_singular_name %>'].id
+	  <%- end -%>
+	  end
+	end
 end
