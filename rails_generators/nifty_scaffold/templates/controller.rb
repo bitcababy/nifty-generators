f87661e62a651ee007d97f01da9437fe348bb82f
Meredith Lesly <bitcababy+github@gmail.com>
2011-08-20 12:07:08 -0400

    Added before_filter and find_ method

--- rails_generators/nifty_scaffold/templates/controller.rb
+++ rails_generators/nifty_scaffold/templates/controller.rb
@@ -1,3 +1,11 @@
 class <%= plural_class_name %>Controller < ApplicationController
+	before_filter :find_<%= instance_name %>, :only => [:show, :edit, :destroy]
   <%= controller_methods :actions %>
+
+	private
+
+		def find_<%= instance_name %>
+			@course = <%= class_name %>.find(params[:id]) if params[:id]
+		end
+
 end
